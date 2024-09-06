1b3511d830267b53fc5fede693f34dcb03a69b00
Francis James Tolentino <francistolentino1107@gmail.com>
2024-09-06 16:36:15 +0800

    Set Default values

--- app/src/main/java/com/universitinder/app/models/School.kt
+++ app/src/main/java/com/universitinder/app/models/School.kt
@@ -1,17 +1,17 @@
 package com.universitinder.app.models
 
 class School (
-    val name: String,
-    val email: String,
-    val contactNumber: String,
-    val address: String,
-    val minimum: Int,
-    val maximum: Int,
-    val affordability: Int,
-    val mission: String,
-    val vision: String,
-    val coreValues: String,
-    val courses: String,
+    val name: String = "",
+    val email: String = "",
+    val contactNumber: String = "",
+    val address: String = "",
+    val minimum: Int = 0,
+    val maximum: Int = 0,
+    val affordability: Int = 0,
+    val mission: String = "",
+    val vision: String = "",
+    val coreValues: String = "",
+    val courses: String = "",
 ) {
     fun asMap() : Map<String, Any> {
         return hashMapOf(
