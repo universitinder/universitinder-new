39ae7afc4422b27c16605811bdf61920552e7710
Francis James Tolentino <francistolentino1107@gmail.com>
2024-09-11 23:26:38 +0800

    Created getFilteredSchool to be used in home screen

--- app/src/main/java/com/universitinder/app/controllers/SchoolController.kt
+++ app/src/main/java/com/universitinder/app/controllers/SchoolController.kt
@@ -1,15 +1,70 @@
 package com.universitinder.app.controllers
 
-import android.util.Log
+import android.net.Uri
+//import android.util.Log
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.firestore
 import com.google.firebase.firestore.toObject
+import com.google.firebase.storage.storage
+import com.universitinder.app.models.Filter
 import com.universitinder.app.models.School
+import com.universitinder.app.models.SchoolPlusImages
 import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
 class SchoolController {
     private val firestore = Firebase.firestore
+    private val storage = Firebase.storage
+
+    suspend fun getFilteredSchool(filter: Filter) : List<SchoolPlusImages> {
+//        Log.w("SCHOOL CONTROLLER", filter.toString())
+        val schools = CompletableDeferred<List<SchoolPlusImages>>()
+        val filteredSchools = CompletableDeferred<List<DocumentSnapshot>>()
+
+        coroutineScope {
+            launch(Dispatchers.IO) {
+                async {
+                    firestore.collectionGroup("school")
+                        .get()
+                        .addOnSuccessListener { objects ->
+//                            Log.w("SCHOOL CONTROLLER", objects.documents.toString())
+                            val filtered = objects.documents.filter { document ->
+                                val school = document.toObject(School::class.java)
+                                filter.provinces.contains(school?.province!!) && filter.cities.contains(school.municipalityOrCity) &&
+                                filter.courses.split("___").intersect(school.courses.toSet()).isNotEmpty()
+                            }
+//                            Log.w("SCHOOL CONTROLLER", filtered.toString())
+                            filteredSchools.complete(filtered)
+                        }
+                        .addOnFailureListener { filteredSchools.complete(emptyList()) }
+                }.await()
+                async {
+                    val filtered = filteredSchools.await()
+                    val schoolPlusImages = filtered.map { document ->
+                        val uris = mutableListOf<Uri>()
+                        val id = document.reference.parent.parent?.id
+                        val storageRef = storage.reference
+                        val listOfItems = storageRef.child("users/${id}").listAll().await()
+                        listOfItems.items.forEach {
+                            val downloadURL = it.downloadUrl.await()
+                            uris.plus(downloadURL)
+                        }
+
+                        SchoolPlusImages(school = document.toObject(School::class.java), images = uris)
+                    }
+//                    Log.w("SCHOOL CONTROLLER", schoolPlusImages.toString())
+                    schools.complete(schoolPlusImages)
+                }.await()
+            }
+        }
+
+        return schools.await()
+    }
 
     suspend fun getSchool(email: String) : School? {
         val school = CompletableDeferred<School?>()
@@ -38,7 +93,7 @@ class SchoolController {
             .set(school)
             .addOnSuccessListener { response.complete(true) }
             .addOnFailureListener {
-                Log.w("SCHOOL CONTROLLER", it.message!!)
+//                Log.w("SCHOOL CONTROLLER", it.message!!)
                 response.complete(false)
             }
 
@@ -70,7 +125,7 @@ class SchoolController {
                 )
                 .addOnSuccessListener { response.complete(true) }
                 .addOnFailureListener {
-                    Log.w("SCHOOL CONTROLLER", it.message!!)
+//                    Log.w("SCHOOL CONTROLLER", it.message!!)
                     response.complete(false)
                 }
         } else {
