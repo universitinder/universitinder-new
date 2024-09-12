de461701b08cf5fbb7c18d90f28819e13f18b15c
Francis James Tolentino <francistolentino1107@gmail.com>
2024-09-13 00:51:25 +0800

    Used ImageCarousel in SwipeableCard

--- app/src/main/java/com/universitinder/app/components/SwipeableCard.kt
+++ app/src/main/java/com/universitinder/app/components/SwipeableCard.kt
@@ -5,15 +5,23 @@ import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Colors
+import androidx.compose.material.IconButton
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +29,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
@@ -73,19 +83,69 @@ fun SwipeableCard(
                 )
             }
             .offset { IntOffset(swipeOffsetX.value.toInt(), 0) }
-            .padding(16.dp)
-//            .paint(painter=rememberAsyncImagePainter(school.images.first()))
-            .background(MaterialTheme.colorScheme.primaryContainer, RoundedCornerShape(16.dp))
-            .fillMaxWidth()
-            .height(500.dp),
-        contentAlignment = Alignment.Center
+            .background(MaterialTheme.colorScheme.primaryContainer),
+        contentAlignment = Alignment.TopCenter
     ) {
-        Column {
-            Text(
-                text = school.school?.name ?: "No Institution Name",
-                fontSize = 32.sp,
-                fontWeight = FontWeight.Bold
-            )
+//        Box(
+//            modifier = Modifier.fillMaxSize(),
+//            contentAlignment = Alignment.TopCenter
+//        ) {
+        ImageCarousel(imageUris = school.images)
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween
+        ){
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(100.dp)
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color(10, 10, 10, 130),
+                                Color.Transparent
+                            )
+                        )
+                    )
+            ) {
+                Text(
+                    modifier = Modifier.padding(16.dp),
+                    text = school.school?.name ?: "No Institution Name",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight.Medium,
+                    color = Color.White
+                )
+            }
+            Row(
+                modifier = Modifier.fillMaxWidth()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color(10, 10, 10, 130),
+                            )
+                        )
+                    ),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ){
+                Box(
+                    modifier = Modifier.padding(24.dp).clip(CircleShape)
+                        .background(Color.White),
+                ) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Close, "Close")
+                    }
+                }
+                Box(
+                    modifier = Modifier.padding(24.dp).clip(CircleShape)
+                        .background(Color.White),
+                ) {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(Icons.Filled.Check, "Check")
+                    }
+                }
+            }
         }
     }
+//    }
 }
