7e899cd072a663c8346f422b4fbfd8eb65d45074
Francis James Tolentino <francistolentino1107@gmail.com>
2024-09-16 15:28:34 +0800

    Removed unused import

--- app/src/main/java/com/universitinder/app/components/SwipeableCard.kt
+++ app/src/main/java/com/universitinder/app/components/SwipeableCard.kt
@@ -16,7 +16,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Close
