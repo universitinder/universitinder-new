b4723dcd881942570ff95473e51b4b31a6c2aa63
Francis James Tolentino <francistolentino1107@gmail.com>
2024-09-16 16:10:13 +0800

    Created MatchesUiState. Initial matches screen UI implementation

--- app/src/main/java/com/universitinder/app/matches/MatchesScreen.kt
+++ app/src/main/java/com/universitinder/app/matches/MatchesScreen.kt
@@ -1,17 +1,50 @@
 package com.universitinder.app.matches
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MatchesScreen(matchesViewModel: MatchesViewModel) {
-    Scaffold { innerPadding ->
-        LazyColumn(
-            modifier = Modifier.padding(innerPadding)
-        ){
+    val uiState by matchesViewModel.uiState.collectAsState()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = { Text(text = "Matches") })
+        }
+    ){ innerPadding ->
+        when (uiState.fetchingLoading) {
+            true -> {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    CircularProgressIndicator()
+                }
+            }
+            false -> {
+                LazyColumn(
+                    modifier = Modifier.padding(innerPadding)
+                ){
+                    itemsIndexed(uiState.matches) { index, school ->
+                        ListItem(
+                            leadingContent = { Text(text = (index+1).toString()) },
+                            headlineContent = { Text(text = school)}
+                        )
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/com/universitinder/app/matches/MatchesViewModel.kt
+++ app/src/main/java/com/universitinder/app/matches/MatchesViewModel.kt
@@ -1,6 +1,41 @@
 package com.universitinder.app.matches
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.universitinder.app.controllers.UserController
+import com.universitinder.app.helpers.ActivityStarterHelper
+import com.universitinder.app.models.UserState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class MatchesViewModel : ViewModel(){
+class MatchesViewModel(
+    private val userController: UserController,
+    private val activityStarterHelper: ActivityStarterHelper
+): ViewModel() {
+    private val currentUser = UserState.currentUser
+    private val _uiState = MutableStateFlow(MatchesUiState())
+    val uiState : StateFlow<MatchesUiState> = _uiState.asStateFlow()
+
+    init {
+        refresh()
+    }
+
+    fun refresh() {
+        if (currentUser != null) {
+            viewModelScope.launch(Dispatchers.IO) {
+                withContext(Dispatchers.Main) { _uiState.value = _uiState.value.copy(fetchingLoading = true) }
+                val schools = userController.getMatchedSchools(currentUser)
+                withContext(Dispatchers.Main) { _uiState.value = _uiState.value.copy(fetchingLoading = false, matches = schools) }
+
+            }
+        }
+    }
+
+    fun startMatchedSchool() {
+
+    }
 }
