eaaea03c8b7a48cb7d6507eb6cdd0113efe564a2
Francis James Tolentino <francistolentino1107@gmail.com>
2024-09-20 22:37:52 +0800

    Added minimum and maximum to model

--- app/src/main/java/com/universitinder/app/models/Filter.kt
+++ app/src/main/java/com/universitinder/app/models/Filter.kt
@@ -3,6 +3,8 @@ package com.universitinder.app.models
 data class Filter(
     val provinces: String = "",
     val cities: String = "",
+    val minimum: Int = 0,
+    val maximum: Int = 0,
     val affordability: Int = 0,
     val courses: String = ""
 )
