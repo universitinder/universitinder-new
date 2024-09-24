4da4d952e3140e2bdc0c67830fd21b693cf849ca
Francis James Tolentino <francistolentino1107@gmail.com>
2024-09-24 22:54:05 +0800

    Deleted Activities, ViewModel, and UiStates of Independent Filters. Put together into one file. Used FilterListWithCheckbox

--- app/src/main/java/com/universitinder/app/filters/affordability/AffordabilityFilterScreen.kt
+++ app/src/main/java/com/universitinder/app/filters/affordability/AffordabilityFilterScreen.kt
@@ -1,85 +1,53 @@
 package com.universitinder.app.filters.affordability
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.universitinder.app.components.AffordabilityIndicator
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AffordabilityFilterScreen(affordabilityFilterViewModel: AffordabilityFilterViewModel) {
-    val uiState by affordabilityFilterViewModel.uiState.collectAsState()
+fun AffordabilityFilterScreen(
+    minimum: Int,
+    onMinimumChange: (newVal: String) -> Unit,
+    maximum: Int,
+    onMaximumChange: (newVal: String) -> Unit,
+    affordability: Int,
+) {
+    Column(
+        modifier = Modifier.padding(20.dp).fillMaxSize()
+    ){
+        OutlinedTextField(
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp),
+            label = { Text(text = "Minimum Price Range") },
+            value = minimum.toString(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = onMinimumChange
+        )
+        OutlinedTextField(
+            singleLine = true,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 10.dp),
+            label = { Text(text = "Maximum Price Range") },
+            value = maximum.toString(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = onMaximumChange
+        )
 
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                navigationIcon = { IconButton(onClick = { affordabilityFilterViewModel.popActivity() }) {
-                    Icon(Icons.Filled.ArrowBack, contentDescription = "Go Back")
-                }
-                },
-                title = { Text(text = "Affordability") }
-            )
-        },
-        bottomBar = {
-            BottomAppBar {
-                Button(modifier = Modifier.fillMaxWidth(), onClick = affordabilityFilterViewModel::save) {
-                    if (uiState.loading) {
-                        CircularProgressIndicator(color = MaterialTheme.colorScheme.onPrimary)
-                    } else {
-                        Text(text = "Save")
-                    }
-                }
-            }
-        }
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier.padding(innerPadding).padding(20.dp)
-        ){
-            OutlinedTextField(
-                singleLine = true,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 10.dp),
-                label = { Text(text = "Minimum Price Range") },
-                value = uiState.minimum.toString(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                onValueChange = affordabilityFilterViewModel::onMinimumChange
-            )
-            OutlinedTextField(
-                singleLine = true,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 10.dp),
-                label = { Text(text = "Maximum Price Range") },
-                value = uiState.maximum.toString(),
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                onValueChange = affordabilityFilterViewModel::onMaximumChange
-            )
-
-            Column {
-                Text(text = "Affordability")
-                AffordabilityIndicator(affordability = uiState.affordability)
-            }
+        Column {
+            Text(text = "Affordability")
+            AffordabilityIndicator(affordability = affordability)
         }
     }
 }
--- app/src/main/java/com/universitinder/app/filters/courses/CoursesFilterScreen.kt
+++ app/src/main/java/com/universitinder/app/filters/courses/CoursesFilterScreen.kt
@@ -1,66 +1,17 @@
 package com.universitinder.app.filters.courses
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
-import androidx.compose.material3.Checkbox
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
+import com.universitinder.app.components.FilterListWithCheckbox
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CoursesFilterScreen(coursesFilterViewModel: CoursesFilterViewModel) {
-    val uiState by coursesFilterViewModel.uiState.collectAsState()
-
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                navigationIcon = { IconButton(onClick = { coursesFilterViewModel.popActivity() }) {
-                    Icon(Icons.Filled.ArrowBack, contentDescription = "Go Back")
-                }
-                },
-                title = { Text(text = "Courses Filter") }
-            )
-        },
-        bottomBar = {
-            BottomAppBar {
-                Button(modifier = Modifier.fillMaxWidth(), onClick = coursesFilterViewModel::save) {
-                    if (uiState.loading) {
-                        CircularProgressIndicator(color = MaterialTheme.colorScheme.onPrimary)
-                    } else {
-                        Text(text = "Save")
-                    }
-                }
-            }
-        }
-    ){ innerPadding ->
-        LazyColumn(
-            modifier = Modifier.padding(innerPadding)
-        ){
-            items(uiState.courses) {course ->
-                ListItem(
-                    modifier = Modifier.clickable { coursesFilterViewModel.onCheckChange(course) },
-                    headlineContent = { Text(text = course) },
-                    trailingContent = { Checkbox(checked = uiState.checkedCourses.contains(course), onCheckedChange = { coursesFilterViewModel.onCheckChange(course) }) }
-                )
-            }
-        }
-    }
+fun CoursesFilterScreen(
+    courses: List<String>,
+    checkedCourses: List<String>,
+    onCourseCheckChange: (course: String) -> Unit
+) {
+    FilterListWithCheckbox(
+        values = courses,
+        checkedValues = checkedCourses,
+        onCheckChange = onCourseCheckChange
+    )
 }
