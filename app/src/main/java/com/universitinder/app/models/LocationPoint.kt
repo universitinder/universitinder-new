b557aa87e9e353f4cc97e99861b491725e9ee304
Francis James Tolentino <francistolentino1107@gmail.com>
2024-09-28 21:23:48 +0800

    Created LocationPoint class

---
+++ app/src/main/java/com/universitinder/app/models/LocationPoint.kt
@@ -0,0 +1,21 @@
+package com.universitinder.app.models
+
+import android.os.Parcelable
+import com.google.firebase.firestore.GeoPoint
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class LocationPoint(
+    val latitude: Double,
+    val longitude: Double,
+) : Parcelable {
+    fun toGeoPoint(): GeoPoint {
+        return GeoPoint(latitude, longitude)
+    }
+
+    companion object {
+        fun fromGeoPoint(geoPoint: GeoPoint): LocationPoint {
+            return LocationPoint(geoPoint.latitude, geoPoint.longitude)
+        }
+    }
+}
