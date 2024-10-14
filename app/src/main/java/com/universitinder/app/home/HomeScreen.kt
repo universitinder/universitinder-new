6e2a28b19fb6a1509687ef6464c9ef77aebfec75
Francis James Tolentino <francistolentino1107@gmail.com>
2024-10-14 23:22:19 +0800

    IDK Random Fixes

--- app/src/main/java/com/universitinder/app/home/HomeScreen.kt
+++ app/src/main/java/com/universitinder/app/home/HomeScreen.kt
@@ -4,24 +4,23 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,7 +31,7 @@ import com.universitinder.app.components.SwipeableCard
 import compose.icons.FeatherIcons
 import compose.icons.feathericons.Filter
 
-@OptIn(ExperimentalPermissionsApi::class)
+@OptIn(ExperimentalPermissionsApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(homeViewModel: HomeViewModel) {
     val uiState by homeViewModel.uiState.collectAsState()
@@ -44,7 +43,20 @@ fun HomeScreen(homeViewModel: HomeViewModel) {
     )
 
     Scaffold(
-        topBar = {  },
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Home") },
+                actions = {
+                    Column(
+                        modifier = Modifier.padding(horizontal = 8.dp).clickable { homeViewModel.startFilterActivity() },
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ){
+                        Icon(FeatherIcons.Filter, contentDescription = "Filter")
+                        Text(text = "Filters", fontSize = 12.sp)
+                    }
+                }
+            )
+        },
     ){ innerPadding ->
         when (fineLocationPermissionState.status) {
             PermissionStatus.Granted -> {
@@ -90,20 +102,20 @@ fun HomeScreen(homeViewModel: HomeViewModel) {
                                     }
                                 }
                             }
-                            Row(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(top = 12.dp, end = 10.dp),
-                                horizontalArrangement = Arrangement.End
-                            ) {
-                                Column(
-                                    modifier = Modifier.clickable { homeViewModel.startFilterActivity() },
-                                    horizontalAlignment = Alignment.CenterHorizontally
-                                ){
-                                    Icon(FeatherIcons.Filter, contentDescription = "Filter", tint = Color.White)
-                                    Text(text = "Filters", fontSize = 12.sp, color = Color.White)
-                                }
-                            }
+//                            Row(
+//                                modifier = Modifier
+//                                    .fillMaxWidth()
+//                                    .padding(top = 12.dp, end = 10.dp),
+//                                horizontalArrangement = Arrangement.End
+//                            ) {
+//                                Column(
+//                                    modifier = Modifier.clickable { homeViewModel.startFilterActivity() },
+//                                    horizontalAlignment = Alignment.CenterHorizontally
+//                                ){
+//                                    Icon(FeatherIcons.Filter, contentDescription = "Filter", tint = Color.White)
+//                                    Text(text = "Filters", fontSize = 12.sp, color = Color.White)
+//                                }
+//                            }
                         }
                     }
                 }
