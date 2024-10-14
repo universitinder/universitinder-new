88de79504510c377cd82faa66a60c58bcd186c8b
Francis James Tolentino <francistolentino1107@gmail.com>
2024-10-14 22:09:05 +0800

    Added Logo in topBar

--- app/src/main/java/com/universitinder/app/school/list/SchoolListScreen.kt
+++ app/src/main/java/com/universitinder/app/school/list/SchoolListScreen.kt
@@ -1,9 +1,14 @@
 package com.universitinder.app.school.list
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
@@ -18,20 +23,49 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.universitinder.app.models.School
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SchoolListScreen(schoolListViewModel: SchoolListViewModel) {
     val uiState by schoolListViewModel.uiState.collectAsState()
+    var bitmapState by remember { mutableStateOf<Bitmap?>(null) }
+    val context = LocalContext.current
+
+    LaunchedEffect(Unit) {
+        bitmapState = BitmapFactory.decodeStream(context.assets.open("logo.jpg"))
+    }
 
     Scaffold (
         topBar = {
-            TopAppBar(title = { Text(text = "Schools") })
+            TopAppBar(
+                title = {
+                    Row (verticalAlignment = Alignment.CenterVertically){
+                        if (bitmapState != null) {
+                            Image(
+                                bitmap = bitmapState!!.asImageBitmap(),
+                                contentDescription = "Logo",
+                                modifier = Modifier.size(50.dp).clip(CircleShape),
+                                contentScale = ContentScale.FillWidth
+                            )
+                        }
+                        Text(text = "Schools", modifier = Modifier.padding(start = 12.dp))
+                    }
+                }
+            )
         },
         floatingActionButton = {
             FloatingActionButton(
