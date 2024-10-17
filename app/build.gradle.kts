128b308f8bf06f597077fbd37deaca917396895f
Francis James Tolentino <francistolentino1107@gmail.com>
2024-10-17 11:04:23 +0800

    IDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,6 +91,7 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
 
     implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
     // dependency for firebase authentication
     implementation("com.google.firebase:firebase-auth")
